e296298c79a7f6a7e3e5d68cfc6f13a98cb74d67
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-10 07:57:03 +0600

    interface and type alias

---
+++ src/interface.ts
@@ -0,0 +1,56 @@
+type user = {
+    name:string,
+    age:number
+};
+
+interface IUser{
+    name:string,
+    age:number
+};
+
+const userWithTypeAlias:user = {
+    name:"Type alias",
+    age:21
+}
+
+const userWithInterface:IUser={
+    name:"Interface",
+    age:22
+}
+
+type extendedUser =user & {
+    role:string
+};
+
+interface IExtendedUser extends IUser{
+    role:string
+}
+
+const extendedUserWithTypeElias:extendedUser = {
+    name:"Extended user",
+    age: 25,
+    role:"Extended"
+};
+
+const extendedUserWithInterface:IExtendedUser ={
+    name:"Extender Interface User",
+    age:26,
+    role:"Interface"
+}
+
+
+type addNumTypes =(num1:number, num2:number)=>number;
+
+interface IAddNumber{
+    (num1:number, num2:number):number
+}
+
+const addNum:IAddNumber = (num1, num2)=> num1+num2;
+
+type rollNumbersType = number[];
+
+interface IRollNumbers{
+    [index:number]:number
+}
+
+const rollNumbers:IRollNumbers = [1, 2, 3];
